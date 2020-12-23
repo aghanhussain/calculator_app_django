cbbce96d0acc6cc119cbf85021348ad34cc0e598
haideralishah <headeralishah@gmail.com>
2020-12-23 19:16:14 +0500

    project uploaded on git

---
+++ calc/calculator/calculator/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def calculate(request):
+    result = {'answer': ''}
+    try:
+        num1 = int(request.GET.get('num1'))
+        num2 = int(request.GET.get('num2'))
+        operator = request.GET.get('op')
+
+        
+        if operator == "add":
+            result['answer'] = num1 + num2
+        elif operator == "sub":
+            result['answer'] = num1 - num2
+        elif operator == "mul":
+            result['answer'] = num1 * num2
+        else:
+            result['answer'] = num1 / num2
+    except Exception as e:
+        result['answer'] = str(e)
+
+        
+
+    return render(request, 'index.html', result)
+
